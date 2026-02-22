968215051647d278f7cabde0a1a14b25d957bb9e
NazninChoity <naznin.sultana.choity@g.bracu.ac.bd>
2026-02-22 21:57:12 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Online_Hotel_Management_Website
-Developed hotel booking platform using MERN stack. Designed with TailwindCSS; integrated React+Vite for routing. Implemented Redux and Axios for state management and API communication. 
+# php_mysql_project
---
+++ dashboard.php
@@ -0,0 +1,30 @@
+<?php
+
+require('inc/essentials.php');
+adminLogin();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - Dashboard</title>
+    <?php require('inc/links.php') ?>
+</head>
+<body class="bg-light">
+    
+<?php require('inc/header.php'); ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quas aliquid assumenda debitis amet! Quam quaerat temporibus, autem voluptates animi itaque inventore sit illum quisquam magni. Illum tenetur sunt dolorem maiores reiciendis iste nulla eum dolorum! Dolorem nulla dolore fugiat voluptatem quo eum sit excepturi explicabo? Voluptatibus deserunt animi, maiores nam nihil veritatis. Minima reprehenderit atque iure dolores illum vitae laborum aut ex quibusdam consequatur, similique eligendi, iste et sequi facere molestiae dolorum? Molestias deserunt officia laborum dignissimos aperiam aut ut? Fugiat, reprehenderit obcaecati eos consectetur temporibus nostrum a porro ab quidem, quo, eligendi maiores! Provident voluptatem, expedita deserunt eaque reiciendis, accusamus saepe, adipisci quis vitae tempora distinctio in pariatur enim! Saepe, numquam, unde perspiciatis expedita vitae reprehenderit ad asperiores aliquid vero eum, quis at. Animi dolores corrupti voluptatem at dolore quibusdam quos consequatur veritatis reprehenderit, accusamus ut cumque enim impedit corporis molestias exercitationem quasi? Vero ipsa iure corrupti quos error! Porro commodi atque dolorem consectetur quis. Culpa reprehenderit, ab consequuntur totam nemo quasi, ipsam, vero minima veniam repellendus obcaecati voluptatum ducimus maxime asperiores et? Facere nobis expedita, ipsam te ipsam consequatur iure temporibus repudiandae, accusantium, itaque alias iste voluptatibus saepe recusandae? Cupiditate necessitatibus, incidunt quos omnis neque quaerat iusto consequatur excepturi fuga saepe mollitia eius ullam aut nesciunt, expedita provident a dignissimos deleniti magni in? Adipisci quas corporis magni quam, laboriosam quos. Ad recusandae, quos repellat illum numquam voluptatibus necessitatibus laborum aliquid voluptas sint, sunt neque aspernatur? Est, illo reiciendis illum minus molestias officia corporis natus eum incidunt facilis dolore quis ipsam hic tenetur totam similique quo. Fugit, perferendis sapiente molestias vitae natus enim veritatis earum incidunt, nulla modi a animi facilis. Facere ratione officia, nulla, voluptate et odit iusto corporis, ipsa reprehenderit necessitatibus doloremque dolorem doloribus alias quo dignissimos quis. Vel error quia quidem beatae accusantium perferendis distinctio totam nulla dignissimos, minima vero eaque maxime perspiciatis optio esse exercitationem ducimus, dolore ex cumque saepe hic. Accusamus at obcaecati sunt amet id! Illo qui facere ab laborum, amet voluptatum porro libero suscipit ea sunt magni esse obcaecati quo nemo sequi blanditiis quis. Perspiciatis aliquam quasi nobis eos enim vel similique, sapiente, officiis facilis ducimus odio quo minus cumque velit, voluptate nam magni quidem repellendus! Ab fugiat nemo hic recusandae voluptatum numquam quisquam? Incidunt rerum eius iste nulla dolorem fugiat neque dolorum possimus ipsum necessitatibus corporis sunt ex est iure mollitia ut, nihil laboriosam facilis nemo, nesciunt repudiandae debitis? Atque numquam nihil reprehenderit modi cum eos soluta sint ipsa, accusantium veritatis magni tempore molestiae iste at earum optio corrupti vero nesciunt odio impedit. Illum numquam laudantium animi quis odio quod ut aliquam, quia, officiis optio harum aspernatur consequatur eius quam! Sunt explicabo deleniti exercitationem maiores esse voluptatum perspiciatis beatae inventore veniam velit voluptate, quia, neque sed quisquam? Atque?
+            </div>
+        </div>
+    </div>
+
+ <?php require('inc/scripts.php'); ?>
+</body>
+</html>
